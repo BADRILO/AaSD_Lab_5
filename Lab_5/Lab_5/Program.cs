892681b9c395d858a90d4df9eda3dda4c40f2683
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-05-18 21:59:28 +0300

    Added insertionSort() and printList() functions

---
+++ Lab_5/Lab_5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_5
+{
+    class Program
+    {
+        static void insertionSort(List<int> list)
+        {
+            int current;
+            int insert_position;
+
+            for (int i = 1; i < list.Count; i++)
+            {
+                current = list[i];
+                insert_position = i;
+                
+                while(insert_position != 0 && current < list[insert_position - 1])
+                {
+                    insert_position--;
+                }
+
+                list.RemoveAt(i);
+                list.Insert(insert_position, current);
+            }
+
+        }
+
+        static void printList(List<int> list)
+        {
+            foreach (int item in list)
+            {
+                Console.Write($"{item, -3} ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        //65318724
+        static void Main(string[] args)
+        {
+            List<int> testList = new() { 6, 5, 3, 1, 8, 7, 2, 4 };
+            printList(testList);
+
+            insertionSort(testList);
+            printList(testList);
+        }
+    }
+}
