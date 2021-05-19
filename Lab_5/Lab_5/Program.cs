f6fac25eabe3850c6eca9e535ed30d5aeb6391b2
DESKTOP-II130FV\DIGAICIA_BADRILO <artem.dehtiarov@nure.ua>
2021-05-19 13:39:41 +0300

    Added tests with time measuring and modified ordinaryQuickSort() function

--- Lab_5/Lab_5/Program.cs
+++ Lab_5/Lab_5/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Lab_5
 {
@@ -11,21 +13,40 @@ namespace Lab_5
             {
                 if (end - start < 10)
                 {
-                    insertionSort(list, start, end);
+                    int current;
+                    int insert_position;
+
+                    for (int i = start + 1; i < end; i++)
+                    {
+                        current = list[i];
+                        insert_position = i;
+
+                        while (insert_position != start && current < list[insert_position - 1])
+                        {
+                            insert_position--;
+                        }
+
+                        list.RemoveAt(i);
+                        list.Insert(insert_position, current);
+                    }
                     return;
                 }
 
                 int pivot_index = start;
+                int pivot_value = list[start];
 
                 for (int temp, i = start + 1; i < end; i++)
                 {
-                    if (list[i] <= list[pivot_index])
+                    if (list[i] <= pivot_value)
                     {
-                        temp = list[i];
-                        list.RemoveAt(i);
-                        list.Insert(pivot_index++, temp);
+                        pivot_index++;
+                        temp = list[pivot_index];
+                        list[pivot_index] = list[i];
+                        list[i] = temp;
                     }
                 }
+                list[start] = list[pivot_index];
+                list[pivot_index] = pivot_value;
 
                 rec(start, pivot_index);
                 rec(pivot_index + 1, end);
@@ -44,16 +65,20 @@ namespace Lab_5
                 }
 
                 int pivot_index = start;
+                int pivot_value = list[start];
 
                 for (int temp, i = start + 1; i < end; i++)
                 {
-                    if (list[i] <= list[pivot_index])
+                    if (list[i] <= pivot_value)
                     {
-                        temp = list[i];
-                        list.RemoveAt(i);
-                        list.Insert(pivot_index++, temp);
+                        pivot_index++;
+                        temp = list[pivot_index];
+                        list[pivot_index] = list[i];
+                        list[i] = temp;
                     }
                 }
+                list[start] = list[pivot_index];
+                list[pivot_index] = pivot_value;
                 
                 rec(start, pivot_index);
                 rec(pivot_index + 1, end);
@@ -95,7 +120,7 @@ namespace Lab_5
 
             for (int i = 0; i < length; i++)
             {
-                list.Add(random.Next(1000));
+                list.Add(random.Next(-2000, 2000));
             }
             return list;
         }
@@ -111,11 +136,44 @@ namespace Lab_5
 
         static void Main(string[] args)
         {
-            List<int> testList = generateList(100);
-            printList(testList);
+            Stopwatch stopwatch = new();
+            List<int> testList = generateList(1000);
+            List<int> testList1 = new();
+            List<int> testList2 = new();
 
-            combinedQuickSort(testList);
+            testList1.AddRange(testList);
+            testList2.AddRange(testList);
             printList(testList);
+            printList(testList1);
+            printList(testList2);
+
+
+
+            Console.WriteLine("COMBINED QUICK SORT");
+            stopwatch.Start();
+            combinedQuickSort(testList);
+            stopwatch.Stop();
+            printList(testList);
+            Console.WriteLine("Elapsed time:" + stopwatch.ElapsedTicks + ";\n\n");
+
+
+
+            Console.WriteLine("ORDINARY QUICK SORT");
+            stopwatch.Restart();
+            ordinaryQuickSort(testList1);
+            stopwatch.Stop();
+            printList(testList1);
+            Console.WriteLine("Elapsed time:" + stopwatch.ElapsedTicks + ";\n\n");
+
+
+
+
+            //Console.WriteLine("INSERTION SORT");
+            //stopwatch.Restart();
+            //insertionSort(testList2);
+            //stopwatch.Stop();
+            //printList(testList2);
+            //Console.WriteLine("Elapsed time:" + stopwatch.ElapsedTicks + ";\n\n");
         }
     }
 }
